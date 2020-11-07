102623685e37b3af8f14a6f7d937f13705b04cd5
kyle-mcnaught <60240503+kyle-mcnaught@users.noreply.github.com>
2020-11-06 22:40:25 -0500

    Added UI
    Created counter for Pickups and Win Screen Text

--- Assets/Scripts/coin_collecting.cs
+++ Assets/Scripts/coin_collecting.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class coin_collecting : MonoBehaviour
 {
+	public TextMeshProUGUI countText;
+	public GameObject winTextObject;
+	
+	private int count;
+	
+	void Start()
+    {
+		count = 0;
+        
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+    
+    void SetCountText ()
+   {
+   	countText.text = "Score: " + count.ToString();
+   	}
+   
     
     private void OnTriggerEnter(Collider other)
    {
    		if(other.gameObject.CompareTag("Pickup"))
    		{
       		other.gameObject.SetActive(false);
-      		
+      		count = count + 1;
+      	
+      	 SetCountText();
       	}
+      	 if(other.gameObject.CompareTag("Finish"))
+   		{
+      	Destroy( this.gameObject );
+      	winTextObject.SetActive(true);
+   		}
     }
 }
