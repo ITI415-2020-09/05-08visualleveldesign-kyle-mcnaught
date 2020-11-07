6166dc55e08ebbb0e06954c987ca2f9d41e9c530
kyle-mcnaught <60240503+kyle-mcnaught@users.noreply.github.com>
2020-11-06 20:09:52 -0500

    More Pickups
    Spread pickup objects throughout terrain

---
+++ Assets/Scripts/PlayerAudio.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAudio : MonoBehaviour
+{
+
+	public AudioClip coinSound;
+	
+	public AudioSource audioC;
+	
+    private void OnTriggerEnter(Collider other)
+   {
+   		if(other.gameObject.CompareTag("Pickup"))
+   		{
+      		audioC.PlayOneShot(coinSound);
+      	}
+    }
+}
